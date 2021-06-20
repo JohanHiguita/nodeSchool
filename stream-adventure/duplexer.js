307f873ea1b2494aa7824f19f342c9464c1c0045
johanHiguita <higuitajohan@gmail.com>
2021-06-19 21:00:45 -0500

    duplexer

---
+++ stream-adventure/duplexer.js
@@ -0,0 +1,12 @@
+const { spawn } = require('child_process');
+const duplexer2 = require("duplexer2");
+
+module.exports = function (cmd, args) {
+  // spawn the process and return a single stream
+  const childProc = spawn(cmd, args);
+
+  // joining together the stdin and stdout here
+  const duplex = duplexer2(childProc.stdin, childProc.stdout);
+
+  return duplex;
+}
